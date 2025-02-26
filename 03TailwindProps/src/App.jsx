75e0c3bc8a12123c03c6221fb1a5a4687de67473
kamalpandey27 <kamal0000027@gmail.com>
2025-02-26 13:29:46 +0530

    check tailwind

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-blue-400">Tailwind Props</h1>
+      <h1 className=" text-white text-2xl bg-blue-400">Tailwind Props</h1>
     </>
   );
 }
